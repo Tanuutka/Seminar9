96aa6f213294f5e5cccd3448a65e472ad0c976c5
Tanuutka <Tanuutka@yandex.ru>
2022-11-30 19:28:08 +0300

    Решены задачи

---
+++ Seminar9/Lesson1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] arrayN = ListOfNaturalNumbers(n);
+PrintArray(arrayN);
+
+int[] ListOfNaturalNumbers(int number)
+{
+    int[] naturalArray = new int[number];
+    for (int i = 0; i < naturalArray.Length; i++)
+    {
+        if (number != 0)
+        {
+            naturalArray[i] = number;
+            number--;
+            ListOfNaturalNumbers(number);
+        }
+    }
+    return naturalArray;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write(String.Join(", ", arr));
+}
